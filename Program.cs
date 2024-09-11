ae5faa5a9851a5949fdbfb91de5234dba2a33de6
Raymundo Solis <pepperray01@gmail.com>
2024-09-11 13:08:05 -0600

    Course APIs | Creating custom middleware

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using webapi.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -19,6 +21,10 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+//app.UseWelcomePage();
+
+app.UseTimeMiddleware();
 
 app.MapControllers();
 
